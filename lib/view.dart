1162c7293f93025537ca4adc5cf754047a8f69ac
Nandhitha2001 <nandhitha0987@gmail.com>
2023-05-16 15:06:48 +0530

    Initial commit

---
+++ lib/view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return  scaffold(
+      TextField(decoration: InputDecoration(border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      SizedBox(height:20,),
+
+      TextField(decoration: InputDecoration(border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      SizedBox(height:20,),
+    );
+  }
+}
